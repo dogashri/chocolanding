3f89baa72c080b7131b6e1861675ff1ba2cf8e5f
rishabh <rishabh singh>
2020-11-22 10:02:56 +0530

    .

--- chocolate_finance/src/Components/Unit.js
+++ chocolate_finance/src/Components/Unit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components';
 import {Link} from 'react-scroll';
 import Subtract from '../Assets/Subtract.png';
